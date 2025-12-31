71b367368c505b5ee46f7564c7699b4f1d744284
Sky-winnie <85846347+Sky-winnie@users.noreply.github.com>
2025-12-31 16:34:11 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,22 @@
+import "./globals.css";
+
 export const metadata = {
-  title: 'GHABSA-UHAS Vault',
-  description: 'Official BMB resource repository',
-}
+  title: "GHABSA-UHAS Vault | BMB Resources",
+  description: "Official repository for BMB students at UHAS",
+  icons: {
+    icon: [
+      { url: "/logo.png" },
+    ],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
