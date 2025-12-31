2d3d6a9aa7540f1fe21b59a04c48a2b052b50f6d
Sky-winnie <85846347+Sky-winnie@users.noreply.github.com>
2025-12-31 12:57:28 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'GHABSA-UHAS Vault',
+  description: 'Official BMB resource repository',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
